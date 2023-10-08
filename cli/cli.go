7e8f6b53c1dda2cd50c01a7251c902436974004c
oskar-dah <oskdah-9@student.ltu.se>
2023-10-08 09:29:04 +0000

    Cli working?

---
+++ cli/cli.go
@@ -0,0 +1,73 @@
+package cli
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+// CLI is a struct representing a command-line interface.
+type CLI struct {
+    prompt   string                        // The command prompt displayed to the user.
+    commands map[string]func([]string)    // A map of command names to their handler functions.
+}
+
+// NewCLI is a constructor function for creating a new CLI instance.
+func NewCLI(prompt string) *CLI {
+    return &CLI{
+        prompt:   prompt,
+        commands: make(map[string]func([]string)),
+    }
+}
+
+// AddCommand associates a command name with its handler function.
+func (c *CLI) AddCommand(name string, handler func([]string)) {
+    c.commands[name] = handler
+}
+
+// Start initiates the CLI and enters a loop to process user input.
+func (c *CLI) Start() {
+    reader := bufio.NewReader(os.Stdin)
+
+    for {
+        fmt.Print(c.prompt)                 // Display the command prompt to the user.
+        input, _ := reader.ReadString('\n')  // Read user input.
+        input = strings.TrimSpace(input)     // Remove leading/trailing whitespace.
+        inputParts := strings.Fields(input)  // Split input into words.
+
+        if len(inputParts) == 0 {
+            continue
+        }
+
+        command := strings.ToLower(inputParts[0])  // Extract the first word as the command.
+        args := inputParts[1:]                      // Extract the remaining words as arguments.
+
+        if command == "exit" {
+            if c.confirmExit() {  // Check if the user wants to exit and confirm.
+                return
+            }
+        } else {
+            handler, exists := c.commands[command]  // Look up the handler for the command.
+            if !exists {
+                fmt.Println("Unknown command. Type 'help' for available commands.")
+                continue
+            }
+            handler(args)  // Execute the handler function with arguments.
+        }
+    }
+}
+
+// confirmExit prompts the user to confirm if they want to exit the CLI.
+func (c *CLI) confirmExit() bool {
+    fmt.Print("Are you sure you want to exit (y/n)? ")
+    input := strings.ToLower(c.readInput())  // Read and convert user input to lowercase.
+    return input == "y"  // Return true if the user confirmed with 'y'.
+}
+
+// readInput reads and returns user input from the standard input.
+func (c *CLI) readInput() string {
+    reader := bufio.NewReader(os.Stdin)
+    input, _ := reader.ReadString('\n')
+    return strings.TrimSpace(input)  // Remove leading/trailing whitespace and return the input.
+}
--- main/main.go
+++ main/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"D7024E/logic"
+	"D7024E/cli"
 	"fmt"
 	"net"
 	"os"
 	"time"
+	"strings"
 )
 
 func main() {
@@ -48,6 +50,9 @@ func main() {
 		fmt.Println("QQQCXXXX", netInstance.Kademlia.PrintData())
 	}
 
+	exitCh := make(chan struct{})
+	go startCLI(exitCh, netInstance)
+
 	for {
 		time.Sleep(time.Hour)
 	}
@@ -70,4 +75,54 @@ func getContainerIP() (string, error) {
 	}
 
 	return addr[0].String(), nil
+}
+
+func startCLI(exitCh chan struct{}, network *logic.Network) {
+    myCLI := cli.NewCLI("> ")
+
+	myCLI.AddCommand("ping", func(args []string) {
+        // Handle 'put' command
+        fmt.Println("Executing 'ping' command with args:", args)
+		contact := logic.NewContact(logic.NewKademliaID("27f2d5effb3dcfe4d7bdd17e64a3101226648a51"), "masterNode")
+		network.SendPingMessage(&contact)
+    })
+
+    myCLI.AddCommand("put", func(args []string) {
+		// Handle 'put' command
+		fmt.Println("Executing 'put' command with args:", args)
+	
+		if len(args) < 1 {
+			fmt.Println("No arguments provided for 'put' command!")
+			return
+		}
+	
+		// Convert the first argument to a byte slice and pass to Store
+		dataToStore := strings.Join(args, " ")  // Join with spaces
+		network.Kademlia.Store([]byte(dataToStore))
+	})
+	
+    myCLI.AddCommand("get", func(args []string) {
+        // Handle 'get' command
+        fmt.Println("Executing 'get' command with args:", args)
+    })
+
+	myCLI.AddCommand("print", func(args []string) {
+        // Handle 'get' command
+        fmt.Println("Executing 'print' command with args:", args)
+		fmt.Println(network.Kademlia.PrintData())
+    })
+
+    myCLI.AddCommand("help", func(args []string) {
+        // Handle 'help' command
+        fmt.Println("Available commands: put, get, exit")
+    })
+
+    // Add an exit command to gracefully exit the CLI and terminate the node
+    myCLI.AddCommand("exit", func(args []string) {
+        fmt.Println("Terminating the node...")
+        // Send a signal to the network Goroutine to exit
+        close(exitCh)
+    })
+
+    myCLI.Start()
 }
